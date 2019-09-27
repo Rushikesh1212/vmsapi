db7d7d9561ed224a3f01be760d942bdb30a567d1
Rushikesh Salunkhe <rushikeshsalunkhe101@gmail.com>
2019-09-27 10:34:03 +0530

    Rushikesh changed in sms api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app'); // app file include
 const port = process.env.PORT || 5014;
-
+console.log("port=>",port);
 const server = http.createServer(app);
 
 server.listen(port);
