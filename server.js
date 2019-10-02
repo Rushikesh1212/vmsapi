faa6eb4671116f454d4fa8f2a98e91dc775ac01f
Rushikesh Salunkhe <rushikeshsalunkhe101@gmail.com>
2019-10-02 19:53:45 +0530

    Rushikesh trying for image upload

--- server.js
+++ server.js
@@ -2,9 +2,5 @@ const http 				= require('http');
 const app 				= require('./app'); // app file include
 const globalVariable 	= require('./nodemon.js');
 const port = process.env.PORT || globalVariable.port;
-console.log("port=>",port)
-console.log("port=>",port)
-
 const server = http.createServer(app);
-
 server.listen(port);
