adba441fb81bac9c5a51ca9069fcb392e47db69c
Rushikesh Salunkhe <rushikeshsalunkhe101@gmail.com>
2019-10-02 19:30:45 +0530

    Rushikesh trying for image upload

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const http = require('http');
 const app = require('./app'); // app file include
 const port = process.env.PORT || 5014;
-// console.log("port=>",port);
 const server = http.createServer(app);
-
 server.listen(port);
-
-
